211f606ec3d27c2f9e705039393c25740e4a4918
divymodi1999 <divymodi1999@gmial.com>
2024-03-17 11:09:05 +0530

    Basic code to showcase usage of banked stack pointer

--- Src/main.c
+++ Src/main.c
@@ -23,9 +23,42 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+int fun_add(int a, int b, int c, int d)
+{
+	return a+b+c+d;
+}
+
+__attribute((naked)) void change_sp_to_psp(void)
+{
+	__asm(".equ Stack_start,(0x20000000 + (96 * 1024))");
+	__asm(".equ Stack_PSP_Start,(Stack_start - 512)");
+	__asm("LDR R0,=Stack_PSP_Start");
+	__asm("MSR PSP, R0");
+	__asm("MOV R0, #0x02");
+	__asm("MSR CONTROL, R0");
+	__asm("BX LR");
+}
+
+void generate_exception()
+{
+	__asm("SVC #0x02");
+}
+
 int main(void)
 {
 	printf("Hello World\n");
+	change_sp_to_psp();
+
+	int ret;
+
+	ret = fun_add(1,2,3,4);
+	printf("result = %d\r\n", ret);
+	generate_exception();
     /* Loop forever */
 	for(;;);
+}
+
+void SVC_Handler(void)
+{
+	printf("inside SVC_handler");
 }
